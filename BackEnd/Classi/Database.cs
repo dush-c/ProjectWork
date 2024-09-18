92d7cb7ad3e2b8fae1617dcbb276fd403a42562d
Giovanni <150935390+dush-c@users.noreply.github.com>
2024-09-18 15:06:27 +0200

    Update Database.cs

--- BackEnd/Classi/Database.cs
+++ BackEnd/Classi/Database.cs
@@ -4,13 +4,13 @@ namespace BackEnd.Classi
 {
     public class Database
     {
-        public string ConnectionString = "Data Source=NIUKO-ITS23-16\\SQLEXPRESS;Initial Catalog=DBContiCorrenti;Integrated Security=True";
-        public SqlConnection? sqlConnection { get; set; }
-        public SqlCommand? sqlCommand { get; set; } = null;
+        public string ConnectionString = "Server=tcp:projectworkits.database.windows.net,1433;Initial Catalog=projectwork;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=Active Directory Default;";
+public SqlConnection? sqlConnection { get; set; }
+public SqlCommand? sqlCommand { get; set; } = null;
 
-        public Database()
-        {
-            sqlConnection = new SqlConnection(ConnectionString);
-        }
+public Database()
+{
+    sqlConnection = new SqlConnection(ConnectionString);
+}
     }
 }
