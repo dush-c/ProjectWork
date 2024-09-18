9f6b22d2f05e0080042da45ae2211eb16151c448
dush-c <meneghellogiovanni88@gmail.com>
2024-09-18 09:31:29 +0200

    Aggiornato il nome dell'ID. Non più userID, ma contoCorrenteID

--- BackEnd/Classi/ResponseRegistrazione.cs
+++ BackEnd/Classi/ResponseRegistrazione.cs
@@ -5,7 +5,7 @@
         //valori di default KO e 0
         //si dà per scontato che l'operazione non sia andata a buon fine
         public string? messaggio { get; set; } = "KO";
-        public int? utenteID { get; set; } = 0;
+        public int? contoCorrenteID { get; set; } = 0;
 
     }
 }
