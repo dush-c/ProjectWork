989fff5b7d049e66220ae94bbbb5afd248539cbd
Mattialagro04 <mattia.lagrotteria@itsdigitalacademy.com>
2024-09-18 12:57:28 +0200

    aggiunta conversione in excel

--- BackEnd/Controllers/MovimentiController.cs
+++ BackEnd/Controllers/MovimentiController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BackEnd.Classi;
 using System.Data.SqlClient;
-using BackEnd.Helpers;
-
+using OfficeOpenXml;
 namespace BankEnd.Controllers
 {
     [Route("api/[controller]")]
@@ -16,9 +15,9 @@ namespace BankEnd.Controllers
             _db = new Database(); // Inizializziamo la connessione al database
         }
 
-        // GET: api/Movimenti/UltimiMovimenti/5
-        [HttpGet("UltimiMovimenti/{contoCorrenteID}/{n}")]
-        public ActionResult<IEnumerable<ResponseMovimento>> GetUltimiMovimenti(int contoCorrenteID, int n)
+        // GET: api/Movimenti/5?n=10&format=excel
+        [HttpGet("{contoCorrenteID}")]
+        public IActionResult GetMovimenti(int contoCorrenteID, [FromQuery] int n, [FromQuery] string format = "json")
         {
             List<ResponseMovimento> movimenti = new List<ResponseMovimento>();
 
@@ -26,7 +25,7 @@ namespace BankEnd.Controllers
             {
                 _db.sqlConnection.Open();
 
-                string query = @"SELECT TOP (@n) Data, Importo, NomeCategoria, Saldo
+                string query = @"SELECT TOP (@n) Data, Importo, NomeCategoria, Saldo 
                                  FROM TMovimentiContoCorrente M
                                  JOIN TCategorieMovimenti C ON M.CategoriaMovimentoID = C.CategoriaMovimentoID
                                  WHERE ContoCorrenteID = @ContoCorrenteID
@@ -45,7 +44,7 @@ namespace BankEnd.Controllers
                             {
                                 Data = reader.GetDateTime(0),
                                 Importo = reader.GetDecimal(1),
-                                NomeCategoria = reader.GetString(2), 
+                                NomeCategoria = reader.GetString(2),
                                 Saldo = reader.GetDecimal(3)
                             });
                         }
@@ -57,14 +56,20 @@ namespace BankEnd.Controllers
                     return NotFound($"Nessun movimento trovato per il conto corrente con ID {contoCorrenteID}.");
                 }
 
-                // Chiamiamo l'helper per creare il file Excel
-                var fileBytes = ExcelHelper.CreateExcelFile(movimenti);
-                var fileName = $"Movimenti_ContoCorrente_{contoCorrenteID}.xlsx";
+                // Se il formato è 'excel', restituiamo il file Excel
+                if (format.Equals("excel", StringComparison.OrdinalIgnoreCase))
+                {
+                    string filePath = GenerateExcelFile(movimenti, contoCorrenteID);
+                    byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+                    string fileName = $"Movimenti_ContoCorrente_{contoCorrenteID}.xlsx";
 
-                return File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
-
-
-                //return Ok(movimenti);
+                    return File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+                }
+                else
+                {
+                    // Altrimenti restituiamo i dati in formato JSON
+                    return Ok(movimenti);
+                }
             }
             catch (Exception ex)
             {
@@ -78,7 +83,7 @@ namespace BankEnd.Controllers
 
         // GET: api/Movimenti/CategoriaMovimento/5/1
         [HttpGet("CategoriaMovimento/{contoCorrenteID}/{categoriaID}/{n}")]
-        public ActionResult<IEnumerable<ResponseMovimento>> GetMovimentiPerCategoria(int contoCorrenteID, int categoriaID, int n)
+        public ActionResult<IEnumerable<ResponseMovimento>> GetMovimentiPerCategoria(int contoCorrenteID, int categoriaID, int n, string format = "json")
         {
             List<ResponseMovimento> movimenti = new List<ResponseMovimento>();
 
@@ -117,9 +122,20 @@ namespace BankEnd.Controllers
                     return NotFound($"Nessun movimento trovato per la categoria con ID {categoriaID}.");
                 }
 
-                //ToExcel(movimenti);
+                // Se il formato è 'excel', restituiamo il file Excel
+                if (format.Equals("excel", StringComparison.OrdinalIgnoreCase))
+                {
+                    string filePath = GenerateExcelFile(movimenti, contoCorrenteID);
+                    byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+                    string fileName = $"Movimenti_ContoCorrente_{contoCorrenteID}.xlsx";
 
-                return Ok(movimenti);
+                    return File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+                }
+                else
+                {
+                    // Altrimenti restituiamo i dati in formato JSON
+                    return Ok(movimenti);
+                }
             }
             catch (Exception ex)
             {
@@ -133,7 +149,7 @@ namespace BankEnd.Controllers
 
         // GET: api/Movimenti/MovimentiTraDate/5?dataInizio=2024-01-01&dataFine=2024-12-31
         [HttpGet("MovimentiTraDate/{contoCorrenteID}")]
-        public ActionResult<IEnumerable<ResponseMovimento>> GetMovimentiTraDate(int contoCorrenteID, DateTime dataInizio, DateTime dataFine, int n)
+        public ActionResult<IEnumerable<ResponseMovimento>> GetMovimentiTraDate(int contoCorrenteID, DateTime dataInizio, DateTime dataFine, int n, string format = "json")
         {
             List<ResponseMovimento> movimenti = new List<ResponseMovimento>();
 
@@ -173,9 +189,20 @@ namespace BankEnd.Controllers
                     return NotFound($"Nessun movimento trovato tra {dataInizio.ToShortDateString()} e {dataFine.ToShortDateString()}.");
                 }
 
-                //ToExcel(movimenti);
+                // Se il formato è 'excel', restituiamo il file Excel
+                if (format.Equals("excel", StringComparison.OrdinalIgnoreCase))
+                {
+                    string filePath = GenerateExcelFile(movimenti, contoCorrenteID);
+                    byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+                    string fileName = $"Movimenti_ContoCorrente_{contoCorrenteID}.xlsx";
 
-                return Ok(movimenti);
+                    return File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+                }
+                else
+                {
+                    // Altrimenti restituiamo i dati in formato JSON
+                    return Ok(movimenti);
+                }
             }
             catch (Exception ex)
             {
@@ -185,6 +212,44 @@ namespace BankEnd.Controllers
             {
                 _db.sqlConnection.Close();
             }
+        }
+        // Funzione che genera fisicamente il file Excel sul server
+        private string GenerateExcelFile(List<ResponseMovimento> movimenti, int contoCorrenteID)
+        {
+            // Definisci il percorso dove salvare il file Excel sul server
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "ExcelFiles");
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            string filePath = Path.Combine(folderPath, $"Movimenti_ContoCorrente_{contoCorrenteID}.xlsx");
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Movimenti");
+
+                // Intestazioni
+                worksheet.Cells[1, 1].Value = "Data";
+                worksheet.Cells[1, 2].Value = "Importo";
+                worksheet.Cells[1, 3].Value = "NomeCategoria";
+                worksheet.Cells[1, 4].Value = "Saldo";
+
+                // Dati
+                for (int i = 0; i < movimenti.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = movimenti[i].Data.ToString("yyyy-MM-dd");
+                    worksheet.Cells[i + 2, 2].Value = movimenti[i].Importo;
+                    worksheet.Cells[i + 2, 3].Value = movimenti[i].NomeCategoria;
+                    worksheet.Cells[i + 2, 4].Value = movimenti[i].Saldo;
+                }
+
+                // Salva il file Excel fisicamente sul server
+                FileInfo file = new FileInfo(filePath);
+                package.SaveAs(file);
+            }
+
+            return filePath;
         }
     }
 }
