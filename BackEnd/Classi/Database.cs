da6c1f40cd371e7bf9904004e2bb4b277e110445
Mattialagro04 <mattia.lagrotteria@itsdigitalacademy.com>
2024-09-18 15:00:14 +0200

    Revert "Aggiornato collegamento al db"

--- BackEnd/Classi/Database.cs
+++ BackEnd/Classi/Database.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.Data.SqlClient;
 
 namespace BackEnd.Classi
 {
     public class Database
-    {/*
-      Server=tcp:projectworkits.database.windows.net,1433;Initial Catalog=projectWork;Persist Security Info=False;User
-        ID=giovanni.meneghello@itsdigitalacademy.com;Password=c4&4H.pkn#37;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication="Active Directory Password";*/
-        public string ConnectionString = "Server=tcp:projectworkits.database.windows.net,1433;Initial Catalog=projectwork;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=Active Directory Default;";
+    {
+        public string ConnectionString = "Data Source=NIUKO-ITS23-16\\SQLEXPRESS;Initial Catalog=DBContiCorrenti;Integrated Security=True";
         public SqlConnection? sqlConnection { get; set; }
         public SqlCommand? sqlCommand { get; set; } = null;
 
