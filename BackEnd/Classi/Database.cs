54e363b96426de523df7481fbb5204ec1dd73736
dush-c <meneghellogiovanni88@gmail.com>
2024-09-20 12:00:02 +0200

    Deploy e Aggiornamento
    Aggiunta una table TUtenti. Adattato il codice per funzionare con la nuova logica

--- BackEnd/Classi/Database.cs
+++ BackEnd/Classi/Database.cs
@@ -4,7 +4,8 @@ namespace BackEnd.Classi
 {
     public class Database
     {
-        public string ConnectionString = "Server=tcp:projectworkits.database.windows.net,1433;Initial Catalog=projectwork;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=Active Directory Default;";
+        public string ConnectionString = "Server=tcp:mioserverits.database.windows.net,1433;Initial Catalog=projectworkfinale;Persist Security Info=False;" +
+            "User ID=mioadmin;Password=miapasswordadmin123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public SqlConnection? sqlConnection { get; set; }
         public SqlCommand? sqlCommand { get; set; } = null;
 
