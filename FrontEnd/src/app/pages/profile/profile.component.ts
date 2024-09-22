5b533dccf88ae3e369b1b1b15ed0c737a7e0fdae
Marzio21 <160005486+Marzio21@users.noreply.github.com>
2024-09-22 17:40:36 +0200

    profile-component

---
+++ FrontEnd/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: 'profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+submitTransfer() {
+throw new Error('Method not implemented.');
+}
+transferForm: FormGroup<any> | undefined;
+
+}
