1f19ef88ca6571fe000d4aa8672835f15508a202
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-09-24 12:12:36 +0200

    Login Page Logo

--- FrontEnd/src/app/enviroments/api-url.ts
+++ FrontEnd/src/app/enviroments/api-url.ts
@@ -1,2 +1,2 @@
-//export var APIURL= "https://backend20240920102207.azurewebsites.net/"
-export var APIURL= "http://localhost:3000"
+// export var APIURL= "https://project-work-backend.vercel.app"
+ export var APIURL= "http://localhost:3000"
