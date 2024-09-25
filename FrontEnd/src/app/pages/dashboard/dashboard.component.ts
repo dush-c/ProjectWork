9d2b0b7ca938907663cd97c060da0d44054ca316
stemolti <mystefano95@gmail.com>
2024-09-25 14:41:13 +0200

    Dashboard update

--- FrontEnd/src/app/pages/dashboard/dashboard.component.ts
+++ FrontEnd/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-
+  finalBalance: number | null = null; // Variabile per tenere traccia del saldo
 }
