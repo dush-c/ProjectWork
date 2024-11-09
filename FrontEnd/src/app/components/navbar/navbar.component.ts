fe1191754861b7b747dfa5d89e4da50e64a0eb51
sergiomeneguzzo <sergio03.mene@gmail.com>
2024-11-09 12:25:45 +0100

    Responsive UI

--- FrontEnd/src/app/components/navbar/navbar.component.ts
+++ FrontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,23 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {User} from "../../interfaces/user.entity";
-import {AuthService} from "../../services/auth.service";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from '../../interfaces/user.entity';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   @Output() logout = new EventEmitter<void>();
   @Input() user: User | null = null;
 
+  isMobileMenuOpen = false;
+
   onLogout() {
     this.logout.emit();
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
 }
